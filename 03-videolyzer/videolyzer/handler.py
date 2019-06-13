03b9a15e7266b5336894df1c5f20df2cf4cd26c7
unknown <jayapragash_b@yahoo.com>
2019-06-12 21:46:28 -0700

    Get video Label data from Rekognition

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -1,5 +1,6 @@
 import urllib
 import os
+import json
 import boto3
 
 def start_label_detection(bucket, key):
@@ -17,9 +18,32 @@ def start_label_detection(bucket, key):
     })
 
     print(response)
-
     return
 
+
+def get_video_labels(job_id):
+    rekognition_client = boto3.client('rekognition')
+
+    response = rekognition_client.get_label_detection(JobId=job_id)
+
+    next_token = response.get('NextToken', None)
+
+    while next_token:
+        next_page = rekognition_client.get_label_detection(
+            JobId=job_id,
+            NextToken=next_token
+        )
+
+        next_token = next_page.get('NextToken', None)
+
+        response['Labels'].extend(next_page['Labels'])
+
+    return response
+
+def put_labels_in_db(data, video_name, video_bucket):
+    pass
+
+# Lambda events
 
 def start_processing_video(event, context):
     # print(event)
@@ -33,7 +57,13 @@ def start_processing_video(event, context):
     return
 
 def handle_label_detection(event, context):
+    for record in event['Records']:
+        message = json.loads(record['Sns']['Message'])
+        job_id = message['JobId']
+        s3_object = message['Video']['S3ObjectName']
+        s3_bucket = message['Video']['S3Bucket']
+        response = get_video_labels(job_id)
+        print(response)
+        put_labels_in_db(response, s3_object, s3_bucket)
 
-    print(event)
-
-    return    
+    return
